05ea1718eb6f333a83d425269a7da777fc6a18be
Racheal <orukele_racheal@yahoo.com>
2020-09-25 09:56:59 +0100

    changes

--- src/api/client.js
+++ src/api/client.js
@@ -11,7 +11,7 @@ let http;
   }
 
   http = axios.create({
-    baseURL: 'http://127.0.0.1:3333/api/v1',
+    baseURL: 'https://shara-api.herokuapp.com/api/v1',
     headers: { ...Authorization },
   });
 })();
